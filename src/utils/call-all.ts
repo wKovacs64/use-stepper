08cbbf4946cc5ba0f667d5cebebd182c59882cba
Justin Hall <justin.r.hall@gmail.com>
2020-05-14 16:25:17 -0600

    refactor(utils): be a little more explicit in call-all implementation (#58)

--- src/utils/call-all.ts
+++ src/utils/call-all.ts
@@ -2,7 +2,9 @@ type CallbackFn = (...args: Array<any>) => void;
 
 export function callAll(...fns: Array<CallbackFn | undefined>): CallbackFn {
   const callAllCallbackFnsWithOriginalArgs: CallbackFn = (...args) => {
-    fns.forEach((fn) => typeof fn === 'function' && fn(...args));
+    fns.forEach((fn) => {
+      if (typeof fn === 'function') fn(...args);
+    });
   };
   return callAllCallbackFnsWithOriginalArgs;
 }
