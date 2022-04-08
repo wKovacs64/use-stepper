7b3a12890e1c52c369ea7f006c8ac6190d0f8db6
Justin Hall <justin.r.hall@gmail.com>
2022-04-07 20:35:08 -0600

    test: replace jest with vitest (#151)

--- src/utils/__tests__/call-all.test.ts
+++ src/utils/__tests__/call-all.test.ts
@@ -2,8 +2,8 @@ import { callAll } from '../call-all';
 
 describe('callAll', () => {
   it('calls all functions passed in with the same arguments', () => {
-    const fn1 = jest.fn();
-    const fn2 = jest.fn();
+    const fn1 = vi.fn();
+    const fn2 = vi.fn();
     const args = ['foo', 'bar', 42];
 
     callAll(fn1, fn2)(...args);
