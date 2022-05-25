7ad042d05300fc49aff35ce8ea22de2624d11034
Justin Hall <justin.r.hall@gmail.com>
2022-05-25 09:39:22 -0600

    test: tweak some test descriptions

--- src/utils/__tests__/decimals.test.ts
+++ src/utils/__tests__/decimals.test.ts
@@ -1,14 +1,14 @@
 import { countDecimals, sum } from '../decimals';
 
 describe('decimals', () => {
-  it('should sum two numbers with any number of decimals', () => {
+  it('adds two numbers with any number of decimals', () => {
     expect(sum(0.1, 0.2)).toBe(0.3);
     expect(sum(1, 0.02)).toBe(1.02);
     expect(sum(NaN, 0.02)).toBe(0.02);
     expect(sum(0.02, NaN)).toBe(0.02);
   });
 
-  it('count the number of decimals', () => {
+  it('counts the number of decimals', () => {
     expect(countDecimals(0.0001232)).toBe(7);
     expect(countDecimals('0.000420')).toBe(6);
     expect(countDecimals('5e-6')).toBe(6);
