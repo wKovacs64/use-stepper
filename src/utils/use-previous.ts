3c176efdeb9bfcd0980bc12f19aae268100534f1
Justin Hall <justin.r.hall@gmail.com>
2025-10-18 12:16:19 -0600

    refactor(internal): prefer useState over useRef in usePrevious impl

--- src/utils/use-previous.ts
+++ src/utils/use-previous.ts
@@ -1,9 +1,15 @@
 import * as React from 'react';
 
 export function usePrevious<T>(value: T): T | undefined {
-  const previousValue = React.useRef<T>(undefined);
+  const [previousValue, setPreviousValue] = React.useState<T | undefined>(undefined);
+  const [currentValue, setCurrentValue] = React.useState<T>(value);
+
   React.useEffect(() => {
-    previousValue.current = value;
-  }, [value]);
-  return previousValue.current;
+    if (currentValue !== value) {
+      setPreviousValue(currentValue);
+      setCurrentValue(value);
+    }
+  }, [value, currentValue]);
+
+  return previousValue;
 }
