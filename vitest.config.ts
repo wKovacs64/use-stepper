8c0dc98afb5ea1a687a7e8cc1ea0630a56a13590
Justin Hall <justin.r.hall@gmail.com>
2025-11-07 10:18:26 -0700

    Fix coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,11 @@
-import { defineConfig, configDefaults } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     setupFiles: './setupTests.ts',
     clearMocks: true,
     coverage: {
-      exclude: [
-        ...(configDefaults.coverage.exclude ?? []),
-        'setupTests.ts',
-        '**/index.ts',
-        '**/*.js',
-      ],
+      include: ['src/**/*.ts(x)?'],
       reporter: ['text', 'lcov', 'clover'],
     },
     globals: true,
